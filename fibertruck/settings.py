f22f52d31a72f0cf1700da04f83d86a87b77dd9f
Ismael Alfaro Marín <ismael.alfaromarin@icloud.com>
2026-07-01 11:08:47 +0200

    Add STATICFILES_DIRS for Railway static files collection

--- fibertruck/settings.py
+++ fibertruck/settings.py
@@ -90,7 +90,9 @@ USE_TZ = True
 # Static files
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-# Use simpler whitenoise storage (no manifest) for Railway compatibility
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
