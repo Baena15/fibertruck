47be9c91a1b8ee65aae46ab9c62a1b774eb4cba2
Ismael Alfaro Marín <ismael.alfaromarin@icloud.com>
2026-07-01 12:36:59 +0200

    Use normal migrate with real migrations

--- fibertruck/wsgi.py
+++ fibertruck/wsgi.py
@@ -1,18 +1,17 @@
 """
 WSGI config for fibertruck project.
-Runs migrate --run-syncdb + auto_setup before serving requests.
+Runs migrate + auto_setup before serving requests.
 """
 import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fibertruck.settings')
 
-# Run migrations and auto-setup before creating the WSGI application
 import django
 django.setup()
 
 from django.core.management import call_command
 try:
-    call_command('migrate', '--run-syncdb', verbosity=0)
+    call_command('migrate', verbosity=0)
     call_command('auto_setup', verbosity=0)
 except Exception:
     pass
